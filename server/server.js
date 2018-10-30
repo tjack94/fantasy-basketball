847db4693558e47bfcbb1987ca33c8e84f1e005b
Taylor Jackson <tjack94@hotmail.com>
2018-10-30 07:04:43 -0600

    Setup server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const massive = require('massive')
+const session = require('express-session')
+const cors =require('cors')
+const path = require('path')
+require('dotenv').config()
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static(__dirname + '/build'));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  }))
+
+  massive(process.env.CONNECTION_STRING)
+  .then(db=>{
+      console.log('db Connected')
+      app.set("db", db)
+  })
+  .catch(err => console.log(err))
+
+  const port = process.env.SERVER_PORT || 3002
+
+  app.listen(port, () => {
+      console.log(`Server listening at localhost:${port}`);
+  }); 
