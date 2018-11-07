68ed409fe4579648a565062787c66e767ca4391a
Taylor Jackson <tjack94@hotmail.com>
2018-11-06 19:50:45 -0700

    tested nba package for concept

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { players } from './playersService'
+import nba from 'nba'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      currentPlayer: null,
+      showPlayerStats: false,
+    }
+  }
+  selectPlayer(id){
+    nba.stats.playerInfo( {PlayerID: id} ).then(response=> this.setState({currentPlayer: response.playerHeadlineStats[0], showPlayerStats: true}))
+  }
   render() {
+    const playersList = players.map((player, index)=>{
+      return (
+      <div key= {index} onClick={()=> this.selectPlayer(player.playerId)}>
+      {player.lastName + ', ' + player.firstName}
+      </div>
+      )
+    })
+      const playerStats = this.state.showPlayerStats? <div>Player: {this.state.currentPlayer.playerName} PPG: {this.state.currentPlayer.pts} APG: {this.state.currentPlayer.ast} RPG: {this.state.currentPlayer.reb}</div> : null
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      {playerStats}
+      {playersList}
       </div>
     );
   }
--- src/playersService.js
+++ src/playersService.js
@@ -1,4 +1,4 @@
-[
+export const players = [
     {
       "firstName": "Alex",
       "lastName": "Abrines",
