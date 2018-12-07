c8b62df16c2841fa3f70ec6a5a539e3b5152d269
Taylor Jackson <tjack94@hotmail.com>
2018-12-06 23:01:33 -0700

    added Create User endpoint

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,16 @@
 module.exports ={
   addPlayer: (req, res) => {
     res.sendStatus(200)
+  },
+  createUser: (req,res) => {
+    const {username, password, email, firstName, lastName} = req.body
+    const dbInstance = req.app.get('db')
+
+    dbInstance.create_user([username, password, email, firstName, lastName])
+    .then(()=> { res.status(200).send({message: 'item added to database'})})
+   .catch((err) => {
+    res.status(500).send({ errorMessage: 'Something went wrong!' });
+    console.log(err);
+ })
   }
 }
