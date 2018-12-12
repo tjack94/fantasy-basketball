e63da866903b6bdf55c33d5c0f2104170dc9e69a
Taylor Jackson <tjack94@hotmail.com>
2018-12-11 20:58:10 -0700

    created NavBar

---
+++ src/views/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <button>Sign in</button>
+      </div>
+    )
+  }
+}
