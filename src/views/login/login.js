410c8b8c1478469f20f4b6507c8a76d22a903882
Taylor Jackson <tjack94@hotmail.com>
2018-12-17 21:14:53 -0700

    Added input boxes and change handlers to login Page

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 export default class login extends Component {
+  constructor (props){
+    super(props)
+    this.state= {
+      username: '',
+      password: ''
+    }
+  }
+  handleChange(event, name) {
+		const value = event.target.value;
+		this.setState({ [name]: value });
+	}
   render() {
     return (
       <div>
-        <input type="text" placeholder= 'username'/>
-        <input type="text" placeholder= 'password'/>
+        <input type="text" placeholder= 'username' onChange = {(e)=> this.handleChange(e, "username")} />
+        <input type="password" placeholder= 'password' onChange = {(e)=> this.handleChange(e, "password")}/>
       </div>
     )
   }
